35e94bc7e398c7584b2d104332fd91905ca7447d
zahraa7 <zahraa.knass@hotmail.com>
2022-03-12 16:26:20 +0200

    done‏

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,7 @@ import React from "react";
 function Header(props) {
       return (
         <h1>
-          {/* uncomment the line below to pass the test */}
-          {/* hello from the Header! */}
+         hello from the Header!
         </h1>
       )
 }
