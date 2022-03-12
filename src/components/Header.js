dbd4de87f98db2c1009ad9db4b121cfce72e3658
zahraa7 <zahraa.knass@hotmail.com>
2022-03-12 16:10:11 +0200

    commit tree f60c60dc728dcd0f4ee260c690fb7ae5691cdf4f

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,7 @@ import React from "react";
 function Header(props) {
       return (
         <h1>
-          {/* uncomment the line below to pass the test */}
-          {/* hello from the Header! */}
+          return <h1>hello from the Header!</h1>;
         </h1>
       )
 }
